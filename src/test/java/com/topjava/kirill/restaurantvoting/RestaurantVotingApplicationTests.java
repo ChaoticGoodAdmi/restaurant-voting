1fea94d6584e9aa0f0e7c4157a1cd4a80b0d709f
ChaoticGoodAdmi <imonare@gmail.com>
2020-10-28 21:27:06 +0300

    Initial commit

---
+++ src/test/java/com/topjava/kirill/restaurantvoting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topjava.kirill.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
