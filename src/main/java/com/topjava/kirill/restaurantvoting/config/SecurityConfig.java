9ef2eacac874844816fa4fd20473065fe5260534
ChaoticGoodAdmi <imonare@gmail.com>
2020-12-10 08:14:27 +0300

    made auth stateless

--- src/main/java/com/topjava/kirill/restaurantvoting/config/SecurityConfig.java
+++ src/main/java/com/topjava/kirill/restaurantvoting/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
                 .httpBasic().and()
                 .authorizeRequests()
